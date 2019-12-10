0c64aaf0523cc241b8158dc9bcc4e11b8b35d891
maierfelix <xilefmai@gmail.com>
2019-12-10 10:47:53 +0100

    Upstream

--- generated/0.0.1/win32/src/GPURayTracingAccelerationInstance.cpp
+++ generated/0.0.1/win32/src/GPURayTracingAccelerationInstance.cpp
@@ -12,16 +12,11 @@ Napi::FunctionReference GPURayTracingAccelerationInstance::constructor;
 GPURayTracingAccelerationInstance::GPURayTracingAccelerationInstance(const Napi::CallbackInfo& info) : Napi::ObjectWrap<GPURayTracingAccelerationInstance>(info) {
   Napi::Env env = info.Env();
 
-  printf("000\n");
   this->device.Reset(info[0].As<Napi::Object>(), 1);
-  printf("111\n");
   GPUDevice* device = Napi::ObjectWrap<GPUDevice>::Unwrap(this->device.Value());
-  printf("222\n");
 
   auto descriptor = DescriptorDecoder::GPURayTracingAccelerationInstanceDescriptor(device, info[1].As<Napi::Value>());
-  printf("333\n");
   this->instance = wgpuDeviceCreateRayTracingAccelerationInstance(device->instance, &descriptor);
-  printf("444\n");
 }
 
 GPURayTracingAccelerationInstance::~GPURayTracingAccelerationInstance() {

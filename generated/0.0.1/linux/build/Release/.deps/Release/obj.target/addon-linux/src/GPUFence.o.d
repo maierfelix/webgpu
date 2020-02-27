50aab39c4c00ac1157545c19f7d7f574a09ab142
Felix Maier <xilefmai@gmail.com>
2020-02-27 19:27:08 +0100

    Update linux build

---
+++ generated/0.0.1/linux/build/Release/.deps/Release/obj.target/addon-linux/src/GPUFence.o.d
@@ -0,0 +1,71 @@
+cmd_Release/obj.target/addon-linux/src/GPUFence.o := /usr/bin/clang++ '-DNODE_GYP_MODULE_NAME=addon-linux' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DDAWN_ENABLE_BACKEND_NULL' '-DDAWN_ENABLE_BACKEND_VULKAN' '-DDAWN_NATIVE_SHARED_LIBRARY' '-DDAWN_WIRE_SHARED_LIBRARY' '-DWGPU_SHARED_LIBRARY' '-DNAPI_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/home/user/.cache/node-gyp/13.9.0/include/node -I/home/user/.cache/node-gyp/13.9.0/src -I/home/user/.cache/node-gyp/13.9.0/deps/openssl/config -I/home/user/.cache/node-gyp/13.9.0/deps/openssl/openssl/include -I/home/user/.cache/node-gyp/13.9.0/deps/uv/include -I/home/user/.cache/node-gyp/13.9.0/deps/zlib -I/home/user/.cache/node-gyp/13.9.0/deps/v8/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/webgpu/node_modules/node-addon-api -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/vulkan-headers/include -I../../../../lib/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/shaderc/libshaderc/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc.cc -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc_private.h  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -std=c++14 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -std=c++14 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -MMD -MF ./Release/.deps/Release/obj.target/addon-linux/src/GPUFence.o.d.raw   -c -o Release/obj.target/addon-linux/src/GPUFence.o ../src/GPUFence.cpp
+Release/obj.target/addon-linux/src/GPUFence.o: ../src/GPUFence.cpp \
+  ../src/GPUFence.h ../src/Base.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/webgpu/node_modules/node-addon-api/napi.h \
+  /home/user/.cache/node-gyp/13.9.0/include/node/node_api.h \
+  /home/user/.cache/node-gyp/13.9.0/include/node/js_native_api.h \
+  /home/user/.cache/node-gyp/13.9.0/include/node/js_native_api_types.h \
+  /home/user/.cache/node-gyp/13.9.0/include/node/node_api_types.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/webgpu/node_modules/node-addon-api/napi-inl.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/webgpu/node_modules/node-addon-api/napi-inl.deprecated.h \
+  ../../../../lib/include/GLFW/glfw3.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn/dawn_proc.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/dawn_proc_table.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu_cpp.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn/EnumClassBitmasks.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/DawnNative.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/dawn_native_export.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_wire/WireClient.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_wire/Wire.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_wire/dawn_wire_export.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_wire/WireServer.h \
+  ../src/Utils.h ../src/GPUQueue.h ../src/GPUDevice.h \
+  ../src/BackendBinding.h ../src/DescriptorDecoder.h ../src/GPUAdapter.h \
+  ../src/GPUBuffer.h ../src/GPUTexture.h ../src/GPUTextureView.h \
+  ../src/GPUSampler.h ../src/GPUBindGroupLayout.h \
+  ../src/GPUPipelineLayout.h ../src/GPUBindGroup.h \
+  ../src/GPUShaderModule.h ../src/GPURenderPipeline.h \
+  ../src/GPURayTracingAccelerationContainer.h \
+  ../src/GPURayTracingShaderBindingTable.h \
+  ../src/GPURayTracingPipeline.h
+../src/GPUFence.cpp:
+../src/GPUFence.h:
+../src/Base.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/webgpu/node_modules/node-addon-api/napi.h:
+/home/user/.cache/node-gyp/13.9.0/include/node/node_api.h:
+/home/user/.cache/node-gyp/13.9.0/include/node/js_native_api.h:
+/home/user/.cache/node-gyp/13.9.0/include/node/js_native_api_types.h:
+/home/user/.cache/node-gyp/13.9.0/include/node/node_api_types.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/webgpu/node_modules/node-addon-api/napi-inl.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/webgpu/node_modules/node-addon-api/napi-inl.deprecated.h:
+../../../../lib/include/GLFW/glfw3.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn/dawn_proc.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/dawn_proc_table.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu_cpp.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn/EnumClassBitmasks.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/DawnNative.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/dawn_native_export.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_wire/WireClient.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_wire/Wire.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_wire/dawn_wire_export.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_wire/WireServer.h:
+../src/Utils.h:
+../src/GPUQueue.h:
+../src/GPUDevice.h:
+../src/BackendBinding.h:
+../src/DescriptorDecoder.h:
+../src/GPUAdapter.h:
+../src/GPUBuffer.h:
+../src/GPUTexture.h:
+../src/GPUTextureView.h:
+../src/GPUSampler.h:
+../src/GPUBindGroupLayout.h:
+../src/GPUPipelineLayout.h:
+../src/GPUBindGroup.h:
+../src/GPUShaderModule.h:
+../src/GPURenderPipeline.h:
+../src/GPURayTracingAccelerationContainer.h:
+../src/GPURayTracingShaderBindingTable.h:
+../src/GPURayTracingPipeline.h:
---
+++ generated/0.0.1/linux/build/Release/.deps/Release/obj.target/addon-linux/src/VulkanBinding.o.d
@@ -0,0 +1,25 @@
+cmd_Release/obj.target/addon-linux/src/VulkanBinding.o := /usr/bin/clang++ '-DNODE_GYP_MODULE_NAME=addon-linux' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DDAWN_ENABLE_BACKEND_NULL' '-DDAWN_ENABLE_BACKEND_VULKAN' '-DDAWN_NATIVE_SHARED_LIBRARY' '-DDAWN_WIRE_SHARED_LIBRARY' '-DWGPU_SHARED_LIBRARY' '-DNAPI_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/home/user/.cache/node-gyp/13.9.0/include/node -I/home/user/.cache/node-gyp/13.9.0/src -I/home/user/.cache/node-gyp/13.9.0/deps/openssl/config -I/home/user/.cache/node-gyp/13.9.0/deps/openssl/openssl/include -I/home/user/.cache/node-gyp/13.9.0/deps/uv/include -I/home/user/.cache/node-gyp/13.9.0/deps/zlib -I/home/user/.cache/node-gyp/13.9.0/deps/v8/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/webgpu/node_modules/node-addon-api -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/vulkan-headers/include -I../../../../lib/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/shaderc/libshaderc/include -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc.cc -I/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc_private.h  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -std=c++14 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -std=c++14 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -MMD -MF ./Release/.deps/Release/obj.target/addon-linux/src/VulkanBinding.o.d.raw   -c -o Release/obj.target/addon-linux/src/VulkanBinding.o ../src/VulkanBinding.cpp
+Release/obj.target/addon-linux/src/VulkanBinding.o: \
+  ../src/VulkanBinding.cpp ../src/BackendBinding.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/DawnNative.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/dawn_proc_table.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/dawn_native_export.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/VulkanBackend.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn/dawn_wsi.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/vulkan-headers/include/vulkan/vulkan.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/vulkan-headers/include/vulkan/vk_platform.h \
+  /mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/vulkan-headers/include/vulkan/vulkan_core.h \
+  ../../../../lib/include/GLFW/glfw3.h
+../src/VulkanBinding.cpp:
+../src/BackendBinding.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/DawnNative.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/out/Shared/gen/src/include/dawn/dawn_proc_table.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/dawn_native_export.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn_native/VulkanBackend.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/src/include/dawn/dawn_wsi.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/vulkan-headers/include/vulkan/vulkan.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/vulkan-headers/include/vulkan/vk_platform.h:
+/mnt/c/Users/maier/Desktop/LXSS_SHARED/dawn-ray-tracing/third_party/vulkan-headers/include/vulkan/vulkan_core.h:
+../../../../lib/include/GLFW/glfw3.h:

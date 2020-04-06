6a24cfd0b04fd72d9650f1a3c61319ebe824f314
Felix Maier <xilefmai@gmail.com>
2020-04-06 18:30:45 +0200

    Update linux build

--- generated/0.0.1/linux/build/Release/.deps/Release/obj.target/addon-linux/src/GPUBindGroup.o.d
+++ generated/0.0.1/linux/build/Release/.deps/Release/obj.target/addon-linux/src/GPUBindGroup.o.d
@@ -1,11 +1,11 @@
-cmd_Release/obj.target/addon-linux/src/GPUBindGroup.o := /usr/bin/clang++ '-DNODE_GYP_MODULE_NAME=addon-linux' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DDAWN_ENABLE_BACKEND_NULL' '-DDAWN_ENABLE_BACKEND_VULKAN' '-DDAWN_NATIVE_SHARED_LIBRARY' '-DDAWN_WIRE_SHARED_LIBRARY' '-DWGPU_SHARED_LIBRARY' '-DNAPI_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/root/.cache/node-gyp/13.12.0/include/node -I/root/.cache/node-gyp/13.12.0/src -I/root/.cache/node-gyp/13.12.0/deps/openssl/config -I/root/.cache/node-gyp/13.12.0/deps/openssl/openssl/include -I/root/.cache/node-gyp/13.12.0/deps/uv/include -I/root/.cache/node-gyp/13.12.0/deps/zlib -I/root/.cache/node-gyp/13.12.0/deps/v8/include -I/home/user/Documents/GitHub/webgpu/node_modules/node-addon-api -I/home/user/Documents/GitHub/dawn-ray-tracing/third_party/vulkan-headers/include -I../../../../lib/include -I/home/user/Documents/GitHub/dawn-ray-tracing/src/include -I/home/user/Documents/GitHub/dawn-ray-tracing/out/Shared/gen/src/include -I/home/user/Documents/GitHub/dawn-ray-tracing/third_party/shaderc/libshaderc/include -I/home/user/Documents/GitHub/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc.cc -I/home/user/Documents/GitHub/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc_private.h  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -std=c++14 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -std=c++14 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -MMD -MF ./Release/.deps/Release/obj.target/addon-linux/src/GPUBindGroup.o.d.raw   -c -o Release/obj.target/addon-linux/src/GPUBindGroup.o ../src/GPUBindGroup.cpp
+cmd_Release/obj.target/addon-linux/src/GPUBindGroup.o := /usr/bin/clang++ '-DNODE_GYP_MODULE_NAME=addon-linux' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DDAWN_ENABLE_BACKEND_NULL' '-DDAWN_ENABLE_BACKEND_VULKAN' '-DDAWN_NATIVE_SHARED_LIBRARY' '-DDAWN_WIRE_SHARED_LIBRARY' '-DWGPU_SHARED_LIBRARY' '-DNAPI_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/home/user/.cache/node-gyp/13.12.0/include/node -I/home/user/.cache/node-gyp/13.12.0/src -I/home/user/.cache/node-gyp/13.12.0/deps/openssl/config -I/home/user/.cache/node-gyp/13.12.0/deps/openssl/openssl/include -I/home/user/.cache/node-gyp/13.12.0/deps/uv/include -I/home/user/.cache/node-gyp/13.12.0/deps/zlib -I/home/user/.cache/node-gyp/13.12.0/deps/v8/include -I/home/user/Documents/GitHub/webgpu/node_modules/node-addon-api -I/home/user/Documents/GitHub/dawn-ray-tracing/third_party/vulkan-headers/include -I../../../../lib/include -I/home/user/Documents/GitHub/dawn-ray-tracing/src/include -I/home/user/Documents/GitHub/dawn-ray-tracing/out/Shared/gen/src/include -I/home/user/Documents/GitHub/dawn-ray-tracing/third_party/shaderc/libshaderc/include -I/home/user/Documents/GitHub/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc.cc -I/home/user/Documents/GitHub/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc_private.h  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -std=c++14 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -std=c++14 -fexceptions -Wno-switch -Wno-unused -Wno-uninitialized -MMD -MF ./Release/.deps/Release/obj.target/addon-linux/src/GPUBindGroup.o.d.raw   -c -o Release/obj.target/addon-linux/src/GPUBindGroup.o ../src/GPUBindGroup.cpp
 Release/obj.target/addon-linux/src/GPUBindGroup.o: \
   ../src/GPUBindGroup.cpp ../src/GPUBindGroup.h ../src/Base.h \
   /home/user/Documents/GitHub/webgpu/node_modules/node-addon-api/napi.h \
-  /root/.cache/node-gyp/13.12.0/include/node/node_api.h \
-  /root/.cache/node-gyp/13.12.0/include/node/js_native_api.h \
-  /root/.cache/node-gyp/13.12.0/include/node/js_native_api_types.h \
-  /root/.cache/node-gyp/13.12.0/include/node/node_api_types.h \
+  /home/user/.cache/node-gyp/13.12.0/include/node/node_api.h \
+  /home/user/.cache/node-gyp/13.12.0/include/node/js_native_api.h \
+  /home/user/.cache/node-gyp/13.12.0/include/node/js_native_api_types.h \
+  /home/user/.cache/node-gyp/13.12.0/include/node/node_api_types.h \
   /home/user/Documents/GitHub/webgpu/node_modules/node-addon-api/napi-inl.h \
   /home/user/Documents/GitHub/webgpu/node_modules/node-addon-api/napi-inl.deprecated.h \
   ../../../../lib/include/GLFW/glfw3.h \
@@ -32,10 +32,10 @@ Release/obj.target/addon-linux/src/GPUBindGroup.o: \
 ../src/GPUBindGroup.h:
 ../src/Base.h:
 /home/user/Documents/GitHub/webgpu/node_modules/node-addon-api/napi.h:
-/root/.cache/node-gyp/13.12.0/include/node/node_api.h:
-/root/.cache/node-gyp/13.12.0/include/node/js_native_api.h:
-/root/.cache/node-gyp/13.12.0/include/node/js_native_api_types.h:
-/root/.cache/node-gyp/13.12.0/include/node/node_api_types.h:
+/home/user/.cache/node-gyp/13.12.0/include/node/node_api.h:
+/home/user/.cache/node-gyp/13.12.0/include/node/js_native_api.h:
+/home/user/.cache/node-gyp/13.12.0/include/node/js_native_api_types.h:
+/home/user/.cache/node-gyp/13.12.0/include/node/node_api_types.h:
 /home/user/Documents/GitHub/webgpu/node_modules/node-addon-api/napi-inl.h:
 /home/user/Documents/GitHub/webgpu/node_modules/node-addon-api/napi-inl.deprecated.h:
 ../../../../lib/include/GLFW/glfw3.h:

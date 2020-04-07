6414f02997804f2bbafd043f31aff47d6a865037
Pelle Johnsen <pelle.johnsen@gmail.com>
2020-04-07 12:09:29 +0200

    Remove generated files

---
+++ generated/0.0.1/darwin/build/Release/.deps/Release/obj.target/addon-darwin/src/GPUTexture.o.d
@@ -0,0 +1,71 @@
+cmd_Release/obj.target/addon-darwin/src/GPUTexture.o := /usr/bin/clang++ '-DNODE_GYP_MODULE_NAME=addon-darwin' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DDAWN_ENABLE_BACKEND_NULL' '-DDAWN_ENABLE_BACKEND_METAL' '-DDAWN_NATIVE_SHARED_LIBRARY' '-DDAWN_WIRE_SHARED_LIBRARY' '-DWGPU_SHARED_LIBRARY' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/Users/pelle/Library/Caches/node-gyp/13.12.0/include/node -I/Users/pelle/Library/Caches/node-gyp/13.12.0/src -I/Users/pelle/Library/Caches/node-gyp/13.12.0/deps/openssl/config -I/Users/pelle/Library/Caches/node-gyp/13.12.0/deps/openssl/openssl/include -I/Users/pelle/Library/Caches/node-gyp/13.12.0/deps/uv/include -I/Users/pelle/Library/Caches/node-gyp/13.12.0/deps/zlib -I/Users/pelle/Library/Caches/node-gyp/13.12.0/deps/v8/include -I/Users/pelle/git/webgpu/node_modules/node-addon-api -I../../../../lib/include -I/Users/pelle/git/dawn-ray-tracing/src/include -I/Users/pelle/git/dawn-ray-tracing/out/Shared/gen -I/Users/pelle/git/dawn-ray-tracing/out/Shared/gen/src/include -I/Users/pelle/git/dawn-ray-tracing/third_party/shaderc/libshaderc/include -I/Users/pelle/git/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc.cc -I/Users/pelle/git/dawn-ray-tracing/third_party/shaderc/libshaderc/src/shaderc_private.h  -O3 -gdwarf-2 -mmacosx-version-min=10.10 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -std=c++14 -stdlib=libc++ -MMD -MF ./Release/.deps/Release/obj.target/addon-darwin/src/GPUTexture.o.d.raw   -c -o Release/obj.target/addon-darwin/src/GPUTexture.o ../src/GPUTexture.cpp
+Release/obj.target/addon-darwin/src/GPUTexture.o: ../src/GPUTexture.cpp \
+  ../src/GPUTexture.h ../src/Base.h \
+  /Users/pelle/git/webgpu/node_modules/node-addon-api/napi.h \
+  /Users/pelle/Library/Caches/node-gyp/13.12.0/include/node/node_api.h \
+  /Users/pelle/Library/Caches/node-gyp/13.12.0/include/node/js_native_api.h \
+  /Users/pelle/Library/Caches/node-gyp/13.12.0/include/node/js_native_api_types.h \
+  /Users/pelle/Library/Caches/node-gyp/13.12.0/include/node/node_api_types.h \
+  /Users/pelle/git/webgpu/node_modules/node-addon-api/napi-inl.h \
+  /Users/pelle/git/webgpu/node_modules/node-addon-api/napi-inl.deprecated.h \
+  ../../../../lib/include/GLFW/glfw3.h \
+  /Users/pelle/git/dawn-ray-tracing/src/include/dawn/dawn_proc.h \
+  /Users/pelle/git/dawn-ray-tracing/out/Shared/gen/src/include/dawn/dawn_proc_table.h \
+  /Users/pelle/git/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu.h \
+  /Users/pelle/git/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu_cpp.h \
+  /Users/pelle/git/dawn-ray-tracing/src/include/dawn/EnumClassBitmasks.h \
+  /Users/pelle/git/dawn-ray-tracing/src/include/dawn_native/DawnNative.h \
+  /Users/pelle/git/dawn-ray-tracing/src/include/dawn_native/dawn_native_export.h \
+  /Users/pelle/git/dawn-ray-tracing/src/include/dawn_wire/WireClient.h \
+  /Users/pelle/git/dawn-ray-tracing/src/include/dawn_wire/Wire.h \
+  /Users/pelle/git/dawn-ray-tracing/src/include/dawn_wire/dawn_wire_export.h \
+  /Users/pelle/git/dawn-ray-tracing/src/include/dawn_wire/WireServer.h \
+  ../src/Utils.h ../src/GPUDevice.h ../src/BackendBinding.h \
+  ../src/GPUTextureView.h ../src/DescriptorDecoder.h ../src/GPUAdapter.h \
+  ../src/GPUQueue.h ../src/GPUFence.h ../src/GPUBuffer.h \
+  ../src/GPUSampler.h ../src/GPUBindGroupLayout.h \
+  ../src/GPUPipelineLayout.h ../src/GPUBindGroup.h \
+  ../src/GPUShaderModule.h ../src/GPURenderPipeline.h \
+  ../src/GPURayTracingAccelerationContainer.h \
+  ../src/GPURayTracingShaderBindingTable.h \
+  ../src/GPURayTracingPipeline.h
+../src/GPUTexture.cpp:
+../src/GPUTexture.h:
+../src/Base.h:
+/Users/pelle/git/webgpu/node_modules/node-addon-api/napi.h:
+/Users/pelle/Library/Caches/node-gyp/13.12.0/include/node/node_api.h:
+/Users/pelle/Library/Caches/node-gyp/13.12.0/include/node/js_native_api.h:
+/Users/pelle/Library/Caches/node-gyp/13.12.0/include/node/js_native_api_types.h:
+/Users/pelle/Library/Caches/node-gyp/13.12.0/include/node/node_api_types.h:
+/Users/pelle/git/webgpu/node_modules/node-addon-api/napi-inl.h:
+/Users/pelle/git/webgpu/node_modules/node-addon-api/napi-inl.deprecated.h:
+../../../../lib/include/GLFW/glfw3.h:
+/Users/pelle/git/dawn-ray-tracing/src/include/dawn/dawn_proc.h:
+/Users/pelle/git/dawn-ray-tracing/out/Shared/gen/src/include/dawn/dawn_proc_table.h:
+/Users/pelle/git/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu.h:
+/Users/pelle/git/dawn-ray-tracing/out/Shared/gen/src/include/dawn/webgpu_cpp.h:
+/Users/pelle/git/dawn-ray-tracing/src/include/dawn/EnumClassBitmasks.h:
+/Users/pelle/git/dawn-ray-tracing/src/include/dawn_native/DawnNative.h:
+/Users/pelle/git/dawn-ray-tracing/src/include/dawn_native/dawn_native_export.h:
+/Users/pelle/git/dawn-ray-tracing/src/include/dawn_wire/WireClient.h:
+/Users/pelle/git/dawn-ray-tracing/src/include/dawn_wire/Wire.h:
+/Users/pelle/git/dawn-ray-tracing/src/include/dawn_wire/dawn_wire_export.h:
+/Users/pelle/git/dawn-ray-tracing/src/include/dawn_wire/WireServer.h:
+../src/Utils.h:
+../src/GPUDevice.h:
+../src/BackendBinding.h:
+../src/GPUTextureView.h:
+../src/DescriptorDecoder.h:
+../src/GPUAdapter.h:
+../src/GPUQueue.h:
+../src/GPUFence.h:
+../src/GPUBuffer.h:
+../src/GPUSampler.h:
+../src/GPUBindGroupLayout.h:
+../src/GPUPipelineLayout.h:
+../src/GPUBindGroup.h:
+../src/GPUShaderModule.h:
+../src/GPURenderPipeline.h:
+../src/GPURayTracingAccelerationContainer.h:
+../src/GPURayTracingShaderBindingTable.h:
+../src/GPURayTracingPipeline.h:
